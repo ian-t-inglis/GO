b24416b4db3f1c8aab077c6b18fb3ff0f922b441
ian-t-inglis <ian.thedragon.inglis@gmail.com>
2022-08-12 18:53:48 +0100

    Fixing spaces with comments

--- fibonacci.go
+++ fibonacci.go
@@ -3,29 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	//Defining the variables to be used for calculation.
-	//The equation for the Fibonacci sequence is Xn = Xn-1 + Xn-2
-	//Or in simpler terms, the current number is equal to the previous number added to the number before the previous number
-	//In this case, i = Xn, j = Xn-1, and k=Xn-2
-	//iter and input are used purely for the loop
+	// Defining the variables to be used for calculation.
+	// The equation for the Fibonacci sequence is Xn = Xn-1 + Xn-2
+	// Or in simpler terms, the current number is equal to the previous number added to the number before the previous number
+	// In this case, i = Xn, j = Xn-1, and k=Xn-2
+	// iter and input are used purely for the loop
 	i := 1
 	j := 1
 	k := 1
 	iter := 0
 	input := 0
 
-	//This section here takes input from the user
+	// This section here takes input from the user
 	fmt.Print("Enter a positive number: ")
 	fmt.Scan(&input)
 
-	//This for loop will constantly loop back if the input entered is not a positive integer
+	// This for loop will constantly loop back if the input entered is not a positive integer
 	for input <= 0 {
 		fmt.Println("Please enter a positive number, negatives will not work.")
 		fmt.Print("Enter number: ")
 		fmt.Scan(&input)
 	}
 
-	//This is where the fibonacci equation is run to return a number along the sequence
+	// This is where the fibonacci equation is run to return a number along the sequence
 	for iter = 0; iter < input; iter++ {
 		k = j
 		j = i
